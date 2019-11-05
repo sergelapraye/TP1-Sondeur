09103a09232224b2ca925102ddf90ae0837052d2
Serge <serge.lapraye@gmail.com>
2019-11-05 14:55:51 +0100

    commit first

---
+++ TP1.h
@@ -0,0 +1,22 @@
+//---------------------------------------------------------------------------
+
+#ifndef TP1H
+#define TP1H
+//---------------------------------------------------------------------------
+#include <System.Classes.hpp>
+#include <Vcl.Controls.hpp>
+#include <Vcl.StdCtrls.hpp>
+#include <Vcl.Forms.hpp>
+//---------------------------------------------------------------------------
+class TForm1 : public TForm
+{
+__published:	// Composants gérés par l'EDI
+	TButton *Button1;
+private:	// Déclarations utilisateur
+public:		// Déclarations utilisateur
+	__fastcall TForm1(TComponent* Owner);
+};
+//---------------------------------------------------------------------------
+extern PACKAGE TForm1 *Form1;
+//---------------------------------------------------------------------------
+#endif
